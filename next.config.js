2cbac42a3898ad3c0a31697fdbef10cdcbf22513
Johann <johannth88@gmail.com>
2021-03-16 15:10:51 +0000

    database started

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  env: {
+      mongodburl: "mongodb+srv://johTrucks:icejoh123@cluster0.yqsjj.mongodb.net/foodtrucks?retryWrites=true&w=majority",
+  }
+};
---
+++ pages/api/truck.js
@@ -0,0 +1,37 @@
+import connectDB from '../../middleware/mongodb';
+import Truck from '../../schemas/truck';
+
+const handler = async (req, res) => {
+  if (req.method === 'POST') {
+    // Check if name, email or password is provided
+    const { name, email, phone, menu, description, location, tags } = req.body;
+    if (name && email && phone && description && tags) {
+        try {
+          // Hash password to store it in DB
+          // var passwordhash = await bcrypt.sign(password);
+          let truck = new Truck({
+            name,
+            email,
+            phone,
+            menu: menu !== null ? menu : 'No menu yet',
+            description,
+            location,
+            tags
+          });
+          let createdTruck = await truck.save();
+          return res.status(200).send(createdTruck);
+        } catch (error) {
+          return res.status(500).send(error.message);
+        }
+      } else {
+        res.status(422).send('data_incomplete');
+      }
+  } else {
+    res.status(422).send('req_method_not_supported');
+  }
+}
+
+export default connectDB(handler);
+// export default (req, res) => {
+//   res.status(200).json({ name: 'TRUUUCKS' })
+// }
